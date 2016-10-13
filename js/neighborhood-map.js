1dcf2d5405d53c436d103eb5cd339f412dff015b
Rishi Gupta <rishig@mac.com>
2016-10-12 23:24:03 -0700

    Some more cleanup, begin implementing click handler

--- js/neighborhood-map.js
+++ js/neighborhood-map.js
@@ -5,9 +5,10 @@ var markers = new Array;
 var map;
 
 var placeIds = [ "ChIJR6lwAHxskFQRTka_PHbS9TQ", "ChIJ3bYQeY53CEER9LMe4keT1yM", "ChIJe7q4I49skFQRB0reqf3Sl9U", "ChIJLVCIgn1skFQR0MkHaxQGR6Y", "ChIJ8eP5VY9skFQRj_eSuT7hnhM" ];
-
+var yelpIds = [ "lot-no-3-bellevue", "tavern-hall-bellevue-2", "earls-kitchen-bar-bellevue", "chantanee-thai-restaurant-and-bar-bellevue-2", "cactus-restaurants-bellevue"];
 var numPlaces = placeIds.length;
 
+// Knockout bindings
 function BusinessEntry(name) {
   var self = this;
   self.name = name;
@@ -32,11 +33,14 @@ function MapsViewModel() {
 
   });
 
+  self.listItemClick = function() {
+    console.log("listItemClick");
+  }
+
 }
 
 var mapsViewModel = new MapsViewModel();
 ko.applyBindings(mapsViewModel);
-console.log("foo");
 
 
 function detailsCallback(index) {
@@ -52,104 +56,106 @@ function detailsCallback(index) {
 
 function initMap() {
 
-      var bellevue = new google.maps.LatLng(47.6135016, -122.2003407);
+  var bellevue = new google.maps.LatLng(47.6135016, -122.2003407);
 
-      map = new google.maps.Map(document.getElementById('map'), {
-        center: bellevue,
-        zoom: 15
-      });
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: bellevue,
+    zoom: 15
+  });
 
-      service = new google.maps.places.PlacesService(map);
+  service = new google.maps.places.PlacesService(map);
 
-      for (var i = 0; i < numPlaces; i++) {
-        var request = { placeId: placeIds[i] };
-        service.getDetails(request, detailsCallback(i));
+  for (var i = 0; i < numPlaces; i++) {
+    var request = { placeId: placeIds[i] };
+    service.getDetails(request, detailsCallback(i));
+  }
+
+  var randomString = function(length) {
+      var text = "";
+      var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+      for(var i = 0; i < length; i++) {
+          text += possible.charAt(Math.floor(Math.random() * possible.length));
       }
+      return text;
+  }
 
-      var randomString = function(length) {
-          var text = "";
-          var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-          for(var i = 0; i < length; i++) {
-              text += possible.charAt(Math.floor(Math.random() * possible.length));
-          }
-          return text;
-      }
+  function generateNonce() {
+    return (Math.floor(Math.random() * 1e12).toString());
+  }
 
-      function generateNonce() {
-        return (Math.floor(Math.random() * 1e12).toString());
-      }
+  function cb(data) {        
+    //console.log("cb: " + JSON.stringify(data));
+  }
 
-      function cb(data) {        
-        console.log("cb: " + JSON.stringify(data));
-      }
+  function callYelp()
+  {
+    // Taken from https://gist.github.com/mnemonicflow/1b90ef0d294c692d24458b8378054c81
+    var auth = {
+        //
+        // Update with your auth tokens.
+        //
+        consumerKey : "-pWwtJNSnVLOO9Pdg_cMIg",
+        consumerSecret : "yraE9tmTtbEmqpK-JZWr0LNzVAM",
+        accessToken : "BFicTaElfzTEcUwrgS74jCruU_KDYpmH",
+        // This example is a proof of concept, for how to use the Yelp v2 API with javascript.
+        // You wouldn't actually want to expose your access token secret like this in a real application.
+        accessTokenSecret : "OKPP3YR2yp_ZSY5frmE0SgvKrkg",
+        serviceProvider : {
+            signatureMethod : "HMAC-SHA1"
+        }
+    };
+    
+    var terms = 'food';
+    var near = 'San+Francisco';
 
-      function callYelp()
-      {
-        // Taken from https://gist.github.com/mnemonicflow/1b90ef0d294c692d24458b8378054c81
-        var auth = {
-            //
-            // Update with your auth tokens.
-            //
-            consumerKey : "-pWwtJNSnVLOO9Pdg_cMIg",
-            consumerSecret : "yraE9tmTtbEmqpK-JZWr0LNzVAM",
-            accessToken : "BFicTaElfzTEcUwrgS74jCruU_KDYpmH",
-            // This example is a proof of concept, for how to use the Yelp v2 API with javascript.
-            // You wouldn't actually want to expose your access token secret like this in a real application.
-            accessTokenSecret : "OKPP3YR2yp_ZSY5frmE0SgvKrkg",
-            serviceProvider : {
-                signatureMethod : "HMAC-SHA1"
+    var accessor = {
+        consumerSecret : auth.consumerSecret,
+        tokenSecret : auth.accessTokenSecret
+    };
+
+    var parameters = [];
+    parameters.push(['term', terms]);
+    parameters.push(['location', near]);
+    parameters.push(['callback', 'cb']);
+    parameters.push(['oauth_consumer_key', auth.consumerKey]);
+    parameters.push(['oauth_consumer_secret', auth.consumerSecret]);
+    parameters.push(['oauth_token', auth.accessToken]);
+    parameters.push(['oauth_signature_method', 'HMAC-SHA1']);
+    
+    var message = {
+        'action' : 'https://api.yelp.com/v2/search',
+        'method' : 'GET',
+        'parameters' : parameters
+    };
+    
+    OAuth.setTimestampAndNonce(message);
+    OAuth.SignatureMethod.sign(message, accessor);
+
+    var parameterMap = OAuth.getParameterMap(message.parameters);
+        
+    $.ajax({
+        'url' : message.action,
+        'data' : parameterMap,
+        'dataType' : 'jsonp',
+        'jsonpCallback' : 'cb',
+        'cache': true
+    })
+    .done(function(data, textStatus, jqXHR) {
+            console.log('success[' + data + '], status[' + textStatus + '], jqXHR[' + JSON.stringify(jqXHR) + ']');
+        }
+    )
+    .fail(function(jqXHR, textStatus, errorThrown) {
+                        console.log('error[' + errorThrown + '], status[' + textStatus + '], jqXHR[' + JSON.stringify(jqXHR) + ']');
             }
-        };
-        
-        var terms = 'food';
-        var near = 'San+Francisco';
+    );
 
-        var accessor = {
-            consumerSecret : auth.consumerSecret,
-            tokenSecret : auth.accessTokenSecret
-        };
+  }
 
-        var parameters = [];
-        parameters.push(['term', terms]);
-        parameters.push(['location', near]);
-        parameters.push(['callback', 'cb']);
-        parameters.push(['oauth_consumer_key', auth.consumerKey]);
-        parameters.push(['oauth_consumer_secret', auth.consumerSecret]);
-        parameters.push(['oauth_token', auth.accessToken]);
-        parameters.push(['oauth_signature_method', 'HMAC-SHA1']);
-        
-        var message = {
-            'action' : 'https://api.yelp.com/v2/search',
-            'method' : 'GET',
-            'parameters' : parameters
-        };
-        
-        OAuth.setTimestampAndNonce(message);
-        OAuth.SignatureMethod.sign(message, accessor);
-
-        var parameterMap = OAuth.getParameterMap(message.parameters);
-            
-        $.ajax({
-            'url' : message.action,
-            'data' : parameterMap,
-            'dataType' : 'jsonp',
-            'jsonpCallback' : 'cb',
-            'cache': true
-        })
-        .done(function(data, textStatus, jqXHR) {
-                console.log('success[' + data + '], status[' + textStatus + '], jqXHR[' + JSON.stringify(jqXHR) + ']');
-            }
-        )
-        .fail(function(jqXHR, textStatus, errorThrown) {
-                            console.log('error[' + errorThrown + '], status[' + textStatus + '], jqXHR[' + JSON.stringify(jqXHR) + ']');
-                }
-        );
-
-      }
-
-      callYelp();
+  callYelp();
 } 
 
- $(document).ready(function() {
+$(document).ready(function() {
 
-  });
+
+
+});
